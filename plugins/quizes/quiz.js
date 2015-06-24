d62cbe62d58523fc0c30bc4fc4d83ba9973ece4e
Alberto Romo Valverde <zarpeng@gmail.com>
2015-06-24 11:24:53 +0200

    allow new question for logued users only

--- plugins/quizes/quiz.js
+++ plugins/quizes/quiz.js
@@ -1,31 +1,36 @@
+// TODO: createdBy on table, and allow access only based on role (CRUD) and permission over record ownership
 module.exports = {
 	handlerNewQuiz:function(req,res,next){
 		var anchor = this;
-		this.viewVars.layout.title = "Quiz - Nueva Pregunta";
-		var quiz = this.dbHelper.getModel("quiz").build(
-			{pregunta:"pregunta",respuesta:"respuesta",tematica:"temática"}
-		);
-		this.viewVars.newquiz.quiz = quiz;
-		return anchor.parseView({view:"layout",render:true,response:res,vars:anchor.viewVars.layout,partials:[
-			{view:"newquiz",vars:anchor.viewVars.newquiz,linkVar:"body"}
-		]});
+		this.ensureAuthorizedACL({req:req,res:res,next:next,handler:function(req1,res1,next1){
+			anchor.viewVars.layout.title = "Quiz - Nueva Pregunta";
+			var quiz = anchor.dbHelper.getModel("quiz").build(
+				{pregunta:"pregunta",respuesta:"respuesta",tematica:"temática"}
+			);
+			anchor.viewVars.newquiz.quiz = quiz;
+			return anchor.parseView({view:"layout",render:true,response:res,vars:anchor.viewVars.layout,partials:[
+				{view:"newquiz",vars:anchor.viewVars.newquiz,linkVar:"body"}
+			]});
+		}});
 	},
 	handlerCreateQuiz:function(req,res,next){
 		var anchor = this;
 		var quiz = this.dbHelper.getModel("quiz").build(req.body.quiz);
-		quiz.validate().then(function(err){
-			if(err){
-				anchor.viewVars.newquiz.quiz = quiz;
-				anchor.viewVars.layout.errors = err.errors;
-				return anchor.parseView({view:"layout",render:true,response:res,vars:anchor.viewVars.layout,partials:[
-					{view:"newquiz",vars:anchor.viewVars.newquiz,linkVar:"body"}
-				],handler:function(){ anchor.viewVars.layout.errors = ""; }});
-			}else{
-				quiz.save({fields: ["pregunta","respuesta","tematica"]}).then(function(){
-					return res.redirect("/quizes");
-				});
-			}
-		});
+		this.ensureAuthorizedACL({req:req,res:res,next:next,handler:function(req1,res1,next1){
+			quiz.validate().then(function(err){
+				if(err){
+					anchor.viewVars.newquiz.quiz = quiz;
+					anchor.viewVars.layout.errors = err.errors;
+					return anchor.parseView({view:"layout",render:true,response:res,vars:anchor.viewVars.layout,partials:[
+						{view:"newquiz",vars:anchor.viewVars.newquiz,linkVar:"body"}
+					],handler:function(){ anchor.viewVars.layout.errors = ""; }});
+				}else{
+					quiz.save({fields: ["pregunta","respuesta","tematica"]}).then(function(){
+						return res.redirect("/quizes");
+					});
+				}
+			});
+		}});
 	},
 	handlerEditQuiz:function(req,res,next){
 		var anchor = this;
